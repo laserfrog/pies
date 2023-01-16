8ff2bcb8a4ee785efecc847bfa2711a198bcff08
laserfrog <obileo@gmail.com>
2023-01-16 21:42:27 +0000

    used .includes

--- things.js
+++ things.js
@@ -1,7 +1,7 @@
 thingy = ["pie", "cheese", "doughbut", "cheese"]
 var array = []
-for (let thing in thingy){
-  
+for (let thing in thingy) {
+
   var yep = {
     'item': thingy[thing]
   }
@@ -18,7 +18,7 @@ let pies = hello();
 function cheese() {
   return "hi there!";
 
-} 
+}
 let pips = cheese()
 console.log(pies, pips);
 
@@ -38,3 +38,12 @@ console.log(barry.at(-1));
 let str = "42";
 console.log(str);
 console.log(Number.parseInt(str, 10));
+
+const my_pokemon = ['barry', 'garry']
+const theres = 'barry'
+
+if (my_pokemon.includes(theres)) {
+  console.log("you win!")
+} else {
+  console.log("you lose!")
+}
